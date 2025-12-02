5baf3098514148282ad2a4ee59e27376fa26e3c5
Oleg Chernyshov <oleg.chernyshov.960@mail.ru>
2025-12-02 15:28:33 +0300

    Переход на docker для БД

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,42 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { databaseConfig } from './config/database.config';
 import { SpellsModule } from './spells/spells.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CharactersModule } from './characters/characters.module';
+import { User } from './users/entities/user.entity';
+import { Spell } from './spells/entities/spell.entity';
+import { CharacterClass } from './spells/entities/character-class.entity';
+import { Character } from './characters/entities/character.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
-    TypeOrmModule.forRoot(databaseConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const port = configService.get<string>('DB_PORT');
+
+        return {
+          type: 'mysql',
+          host: configService.get<string>('DB_HOST', 'localhost'),
+          port: port ? parseInt(port, 10) : 3306,
+          username: configService.get<string>('DB_USERNAME', 'root'),
+          password:
+            configService.get<string>('DB_PASSWORD') ??
+            configService.get<string>('DB_ROOT_PASSWORD', ''),
+          database: configService.get<string>('DB_DATABASE', 'pocket_spellbook'),
+          entities: [User, Spell, CharacterClass, Character],
+          synchronize: configService.get<string>('NODE_ENV') !== 'production',
+          logging: configService.get<string>('NODE_ENV') === 'development',
+        };
+      },
+      inject: [ConfigService],
+    }),
     SpellsModule,
     UsersModule,
     AuthModule,
